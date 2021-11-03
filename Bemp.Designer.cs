ae3134b8a3e4426b516b93d1732bdbb94523fd58
Samantha Charles <samycharles37@gmail.com>
2021-11-02 21:44:49 -0600

    se agrego alta de empleados

--- Bemp.Designer.cs
+++ Bemp.Designer.cs
@@ -29,7 +29,7 @@ namespace proyectoMAD
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuempleado = new System.Windows.Forms.MenuStrip();
             this.gestiónDeClientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.altasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bajasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -37,37 +37,40 @@ namespace proyectoMAD
             this.desbloqueDeCuentaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.crearTarifaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crearTarifaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignarTarifaAZonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.crearRangoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.crearContratoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.crearContratoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.crearTarifaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.asignarTarifaAZonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reciboToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lecturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.menuStrip1.SuspendLayout();
+            this.gestiónDeZonaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuempleado.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // menuempleado
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuempleado.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.gestiónDeClientesToolStripMenuItem,
-            this.desbloqueDeCuentaToolStripMenuItem,
+            this.crearContratoToolStripMenuItem,
+            this.gestiónDeZonaToolStripMenuItem,
             this.crearTarifaToolStripMenuItem,
             this.crearRangoToolStripMenuItem,
-            this.crearContratoToolStripMenuItem,
             this.reciboToolStripMenuItem,
-            this.lecturaToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(583, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.lecturaToolStripMenuItem,
+            this.desbloqueDeCuentaToolStripMenuItem});
+            this.menuempleado.Location = new System.Drawing.Point(0, 0);
+            this.menuempleado.Name = "menuempleado";
+            this.menuempleado.Size = new System.Drawing.Size(725, 24);
+            this.menuempleado.TabIndex = 0;
+            this.menuempleado.Text = "menuStrip1";
+            this.menuempleado.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
             // 
             // gestiónDeClientesToolStripMenuItem
             // 
@@ -108,7 +111,7 @@ namespace proyectoMAD
             // clienteToolStripMenuItem
             // 
             this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
-            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.clienteToolStripMenuItem.Text = "Cliente";
             // 
             // crearTarifaToolStripMenuItem
@@ -120,11 +123,23 @@ namespace proyectoMAD
             this.crearTarifaToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
             this.crearTarifaToolStripMenuItem.Text = "Tarifa";
             // 
+            // crearTarifaToolStripMenuItem1
+            // 
+            this.crearTarifaToolStripMenuItem1.Name = "crearTarifaToolStripMenuItem1";
+            this.crearTarifaToolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
+            this.crearTarifaToolStripMenuItem1.Text = "Cargar tarifa";
+            // 
+            // asignarTarifaAZonaToolStripMenuItem
+            // 
+            this.asignarTarifaAZonaToolStripMenuItem.Name = "asignarTarifaAZonaToolStripMenuItem";
+            this.asignarTarifaAZonaToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.asignarTarifaAZonaToolStripMenuItem.Text = "Asignar tarifa a zona";
+            // 
             // crearRangoToolStripMenuItem
             // 
             this.crearRangoToolStripMenuItem.Name = "crearRangoToolStripMenuItem";
-            this.crearRangoToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
-            this.crearRangoToolStripMenuItem.Text = "Crear rango";
+            this.crearRangoToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.crearRangoToolStripMenuItem.Text = "Carga de Consumos";
             this.crearRangoToolStripMenuItem.Click += new System.EventHandler(this.crearRangoToolStripMenuItem_Click);
             // 
             // crearContratoToolStripMenuItem
@@ -140,45 +155,34 @@ namespace proyectoMAD
             // crearContratoToolStripMenuItem1
             // 
             this.crearContratoToolStripMenuItem1.Name = "crearContratoToolStripMenuItem1";
-            this.crearContratoToolStripMenuItem1.Size = new System.Drawing.Size(150, 22);
+            this.crearContratoToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
             this.crearContratoToolStripMenuItem1.Text = "Crear contrato";
             // 
             // modificarToolStripMenuItem
             // 
             this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
-            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
-            this.modificarToolStripMenuItem.Text = "Modificar";
-            // 
-            // crearTarifaToolStripMenuItem1
-            // 
-            this.crearTarifaToolStripMenuItem1.Name = "crearTarifaToolStripMenuItem1";
-            this.crearTarifaToolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
-            this.crearTarifaToolStripMenuItem1.Text = "Crear tarifa";
-            // 
-            // asignarTarifaAZonaToolStripMenuItem
-            // 
-            this.asignarTarifaAZonaToolStripMenuItem.Name = "asignarTarifaAZonaToolStripMenuItem";
-            this.asignarTarifaAZonaToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
-            this.asignarTarifaAZonaToolStripMenuItem.Text = "Asignar tarifa a zona";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modificarToolStripMenuItem.Text = "Modificar contrato";
             // 
             // reciboToolStripMenuItem
             // 
             this.reciboToolStripMenuItem.Name = "reciboToolStripMenuItem";
-            this.reciboToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
-            this.reciboToolStripMenuItem.Text = "Recibo";
+            this.reciboToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.reciboToolStripMenuItem.Text = "Recibos";
             // 
             // lecturaToolStripMenuItem
             // 
             this.lecturaToolStripMenuItem.Name = "lecturaToolStripMenuItem";
-            this.lecturaToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
-            this.lecturaToolStripMenuItem.Text = "Lectura";
+            this.lecturaToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.lecturaToolStripMenuItem.Text = "Reportes";
+            this.lecturaToolStripMenuItem.Click += new System.EventHandler(this.lecturaToolStripMenuItem_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(105, 40);
+            this.label1.Location = new System.Drawing.Point(210, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(271, 29);
             this.label1.TabIndex = 2;
@@ -186,7 +190,7 @@ namespace proyectoMAD
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(475, 341);
+            this.button1.Location = new System.Drawing.Point(580, 340);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -196,7 +200,7 @@ namespace proyectoMAD
             // pictureBox1
             // 
             this.pictureBox1.Image = global::proyectoMAD.Properties.Resources._172398;
-            this.pictureBox1.Location = new System.Drawing.Point(147, 90);
+            this.pictureBox1.Location = new System.Drawing.Point(252, 89);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(185, 189);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -208,27 +212,33 @@ namespace proyectoMAD
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(59, 297);
+            this.label2.Location = new System.Drawing.Point(164, 296);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(405, 29);
             this.label2.TabIndex = 5;
             this.label2.Text = "Por favor, elige una opcion del menú";
             // 
-            // Form3
+            // gestiónDeZonaToolStripMenuItem
+            // 
+            this.gestiónDeZonaToolStripMenuItem.Name = "gestiónDeZonaToolStripMenuItem";
+            this.gestiónDeZonaToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.gestiónDeZonaToolStripMenuItem.Text = "Gestión de zona";
+            // 
+            // Bemp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 386);
+            this.ClientSize = new System.Drawing.Size(725, 386);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form3";
+            this.Controls.Add(this.menuempleado);
+            this.MainMenuStrip = this.menuempleado;
+            this.Name = "Bemp";
             this.Text = "Empleado";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.menuempleado.ResumeLayout(false);
+            this.menuempleado.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -237,7 +247,7 @@ namespace proyectoMAD
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuempleado;
         private System.Windows.Forms.ToolStripMenuItem gestiónDeClientesToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem altasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bajasToolStripMenuItem;
@@ -257,5 +267,6 @@ namespace proyectoMAD
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem gestiónDeZonaToolStripMenuItem;
     }
 }
--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,12 +29,154 @@ namespace proyectoMAD
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Por favor ingresa los datos que se piden:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Por favor ingresa los datos que se piden:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Teléfono:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Código Postal:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 281);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Número:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 243);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Ciudad:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(46, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Estado:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(46, 176);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Email:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(46, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Apellido Materno:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(46, 105);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Apellido Paterno:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(46, 66);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(58, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Nombre(s):";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Alta de empelados";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
     }
 }
